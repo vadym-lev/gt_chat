fceecf3c6d6c8fb8f60fe539ab6485e86fe5f534
vadym <vadlevkovskiy@gmail.com>
2024-10-28 19:52:25 +0200

    add postgres container

--- app/db.py
+++ app/db.py
@@ -4,7 +4,8 @@ from sqlalchemy.orm import sessionmaker
 from app.config import DATABASE_URL
 
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
+print(f"++++++++++++++ DATABASE_URL = {DATABASE_URL}")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
